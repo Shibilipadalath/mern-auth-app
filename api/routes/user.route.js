d08aac4d69560c1061f1475b6ceda91be209fe28
Shibilipadalath <shibilipadalath@gmail.com>
2025-01-24 14:48:59 +0530

    create update user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser} from "../controllers/user.controller.js";
+import { verifyUser } from "../utils/VerifyUser.js";
 
 const router = express.Router();
 
 router.get("/", test);
+router.post("/update/:id", verifyUser,updateUser);
 
 
 export default router
