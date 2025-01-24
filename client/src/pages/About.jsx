cda45bc6a3c44d901ab91031cf26fddecf56db77
Shibilipadalath <shibilipadalath@gmail.com>
2025-01-24 16:13:32 +0530

    complete home and aboute pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
 function About() {
   return (
-    <div>
-      About
+    <div className="flex items-start justify-center min-h-screen px-4 py-12 bg-gray-50">
+      <div className="max-w-2xl text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">About Us</h1>
+        <p className="text-lg text-gray-600">
+          Welcome to our authentication app! This platform is designed to
+          provide a secure and seamless experience for managing user accounts,
+          ensuring safety and convenience. Whether it's logging in, signing up,
+          or managing your profile, we've got you covered with robust features
+          and modern technology. Explore our app and experience security like
+          never before.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import {useSelector} from 'react-redux';
 
 function Home() {
+  const {currentUser} = useSelector((state)=>state.user)
   return (
-    <div>
-      Home
+    <div className="flex items-start justify-center min-h-screen px-4 py-12">
+      <div className="max-w-2xl text-center">
+        <h1 className="text-3xl font-bold">Welcome</h1>
+        <p className="text-lg text-gray-600 mt-4">
+          {currentUser?`Welcome, ${currentUser.username}!`:'Welcome to our authentication app!'}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
