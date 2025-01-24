ed052a3bea337a0fdb19c7b8cf103f3971a66ea7
Shibilipadalath <shibilipadalath@gmail.com>
2025-01-24 16:02:40 +0530

    add user signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,7 +8,7 @@ import {
 } from "firebase/storage";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
-import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess } from "../redux/user/userSlice";
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess,signOut } from "../redux/user/userSlice";
 
 function Profile() {
   const dispatch = useDispatch();
@@ -94,6 +94,15 @@ function Profile() {
       dispatch(deleteUserFailure(error));
     }
   }
+
+  const handleSignOut = async () => {
+    try {
+      await fetch('/api/auth/signout');
+      dispatch(signOut());
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -151,7 +160,7 @@ function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteAccount} className=" text-red-700 cursor-pointer">Delete Account</span>
-        <span className=" text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className=" text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error && "Somthing went wrong"}</p>
       <p className="text-green-700 mt-5">{updateSuccess && "User updated successfully"}</p>
